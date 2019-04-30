20e70c6b6d88e62ff93f0e3f2f19ae448abe2418
dgr <reichelt@informatik.uni-leipzig.de>
2019-04-30 16:00:25 +0200

    Initial Commit

---
+++ src/main/java/de/peran/evaluation/GetNonSelectedTests.java
@@ -0,0 +1,66 @@
+package de.peran.evaluation;
+
+import java.io.File;
+import java.io.IOException;
+import java.util.List;
+import java.util.Map;
+
+import org.apache.logging.log4j.LogManager;
+import org.apache.logging.log4j.Logger;
+
+import com.fasterxml.jackson.core.JsonParseException;
+import com.fasterxml.jackson.databind.JsonMappingException;
+import com.fasterxml.jackson.databind.ObjectMapper;
+import com.fasterxml.jackson.databind.SerializationFeature;
+
+import de.peass.DependencyReadingStarter;
+import de.peass.dependency.analysis.data.ChangedEntity;
+import de.peass.dependency.analysis.data.TestSet;
+import de.peass.dependency.persistence.ExecutionData;
+
+public class GetNonSelectedTests {
+
+	private static final Logger LOG = LogManager.getLogger(GetNonSelectedTests.class);
+
+	private static final ObjectMapper MAPPER = new ObjectMapper();
+	static {
+		MAPPER.enable(SerializationFeature.INDENT_OUTPUT);
+	}
+
+	public static void main(String[] args) throws JsonParseException, JsonMappingException, IOException {
+		final File allTestsFile = new File(args[0]);
+		final File selectedTestsFile = new File(args[1]);
+
+		final ExecutionData all = MAPPER.readValue(allTestsFile, ExecutionData.class);
+		final ExecutionData selected = MAPPER.readValue(selectedTestsFile, ExecutionData.class);
+
+		final ExecutionData nonSelected = new ExecutionData();
+		nonSelected.setUrl(all.getUrl());
+		for (final Map.Entry<String, TestSet> allVersion : all.getVersions().entrySet()) {
+			LOG.debug("Version: {}", allVersion.getKey());
+			final TestSet allTestSet = allVersion.getValue();
+			if (selected.getVersions().containsKey(allVersion.getKey())) {
+				final TestSet selectedVersion = selected.getVersions().get(allVersion.getKey());
+				for (final Map.Entry<ChangedEntity, List<String>> selectedTests : selectedVersion.getTestcases().entrySet()) {
+					final String testclazz = selectedTests.getKey().getJavaClazzName();
+					final List<String> remainingTests = allTestSet.getTestcases().get(testclazz);
+					System.out.println("Searching: " + testclazz + " " + remainingTests);
+					if (remainingTests != null) { //TODO Workaround: Test-suite and extended tests are also selected, but shouldn't
+						if (!remainingTests.containsAll(selectedTests.getValue())) {
+							LOG.error("Testclazz " + testclazz);
+							LOG.error("Selected contain more than all: " + remainingTests + " " + selectedTests.getValue());
+						}
+
+						remainingTests.removeAll(selectedTests.getValue());
+						if (remainingTests.size() == 0) {
+							allTestSet.getTestcases().remove(testclazz);
+						}
+					}
+				}
+			}
+			LOG.debug("Size: " + allTestSet.classCount());
+			nonSelected.getVersions().put(allVersion.getKey(), allTestSet);
+		}
+		MAPPER.writeValue(new File(DependencyReadingStarter.getResultFolder(), "out.json"), nonSelected);
+	}
+}
---
+++ src/main/java/de/peran/evaluation/empty/ReadAllTests.java
@@ -0,0 +1,112 @@
+package de.peran.evaluation.empty;
+
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.io.FileWriter;
+import java.io.IOException;
+import java.util.List;
+
+import javax.xml.bind.JAXBException;
+
+import org.apache.commons.cli.ParseException;
+import org.apache.logging.log4j.LogManager;
+import org.apache.logging.log4j.Logger;
+
+import com.fasterxml.jackson.core.JsonParseException;
+import com.fasterxml.jackson.databind.JsonMappingException;
+import com.fasterxml.jackson.databind.ObjectMapper;
+import com.fasterxml.jackson.databind.SerializationFeature;
+import com.github.javaparser.JavaParser;
+import com.github.javaparser.ast.CompilationUnit;
+import com.github.javaparser.ast.body.ClassOrInterfaceDeclaration;
+
+import de.peass.DependencyReadingStarter;
+import de.peass.dependency.ClazzFinder;
+import de.peass.dependency.analysis.data.ChangedEntity;
+import de.peass.dependency.analysis.data.TestCase;
+import de.peass.dependency.persistence.ExecutionData;
+import de.peass.testtransformation.JUnitTestTransformer;
+import de.peass.testtransformation.ParseUtil;
+import de.peass.vcs.GitUtils;
+import de.peran.evaluation.base.Evaluator;
+
+/**
+ * Generates a) Trace-Method-Diff and b) Trace-Method-Source-Diff from a project by loading every version, executing it with instrumentation and afterwards closing it.
+ * 
+ * @author reichelt
+ *
+ */
+public class ReadAllTests extends Evaluator {
+
+   private static final Logger LOG = LogManager.getLogger(ReadAllTests.class);
+
+   private static final ObjectMapper MAPPER = new ObjectMapper();
+   static {
+      MAPPER.enable(SerializationFeature.INDENT_OUTPUT);
+   }
+
+   private final File executeFile;
+   private final ExecutionData changedTraceMethods = new ExecutionData();
+
+   public ReadAllTests(final String args[]) throws ParseException {
+      super("readall", args);
+      final File resultFolder = DependencyReadingStarter.getResultFolder();
+      final String projectName = projectFolder.getName();
+      final String url = GitUtils.getURL(projectFolder);
+      changedTraceMethods.setUrl(url);
+      executeFile = new File(resultFolder, "execute_full_" + projectName + ".json");
+   }
+
+   @Override
+   public void evaluate() {
+      analyzeVersion();
+      while (iterator.hasNextCommit()) {
+         iterator.goToNextCommit();
+         analyzeVersion();
+
+         try (FileWriter fw = new FileWriter(executeFile)) {
+            fw.write(MAPPER.writeValueAsString(changedTraceMethods));
+            fw.flush();
+         } catch (final IOException e) {
+            e.printStackTrace();
+         }
+
+      }
+   }
+
+   private void analyzeVersion() {
+      final String version = iterator.getTag();
+
+      final File srcFolder = new File(projectFolder, "src");
+      final List<String> clazzes = ClazzFinder.getTestClazzes(srcFolder);
+      final File testSrcFolder = ClazzFinder.getTestFolder(srcFolder);
+      for (final String clazzName : clazzes) {
+         final String filename = clazzName.replace(".", File.separator) + ".java";
+         final File clazzFile = new File(testSrcFolder, filename);
+         if (clazzFile.exists()) {
+            try {
+               LOG.trace("Clazz: {}", clazzName);
+               final CompilationUnit unit = JavaParser.parse(clazzFile);
+               final ClassOrInterfaceDeclaration clazz = ParseUtil.getClass(unit);
+               final JUnitTestTransformer transformer = new JUnitTestTransformer(projectFolder);
+               if (clazz != null) { // File could also define @interface instead of class
+                  final List<String> methods = transformer.getTests(projectFolder, new ChangedEntity(clazz.getNameAsString(), ""));
+                  for (final String method : methods) {
+                     final TestCase testcase = new TestCase(clazzName, method);
+                     changedTraceMethods.addCall(version, version + "~1", testcase);
+                  }
+               }
+
+            } catch (final FileNotFoundException e) {
+               e.printStackTrace();
+            }
+         }
+      }
+   }
+
+   public static void main(final String[] args) throws ParseException, JAXBException, JsonParseException, JsonMappingException, IOException {
+      final Evaluator evaluator = new ReadAllTests(args);
+      evaluator.evaluate();
+   }
+
+}
