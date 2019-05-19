432142b1d36fcd3886449e49446dc5a984722f63
David Georg Reichelt <davidgeorg_reichelt@dagere.de>
2019-05-19 14:45:03 +0200

    Aktuelle Namen, Projekt parametrisierbar

--- evaluateAll.sh
+++ evaluateAll.sh
@@ -1,3 +1,6 @@
-java -cp target/evaluation-0.1-SNAPSHOT.jar de.peran.evaluation.infinitest.InfinitestEvaluator -folder ../../projekte/commons-fileupload/ &> fileupload_infinitest.txt
-java -cp target/evaluation-0.1-SNAPSHOT.jar de.peran.evaluation.ekstazi.EkstaziEvaluator -folder ../../projekte/commons-fileupload/ &> fileupload_ekstazi.txt
-java -cp target/evaluation-0.1-SNAPSHOT.jar de.peran.evaluation.empty.EmptyEvaluator -folder ../../projekte/commons-fileupload/ &> fileupload_empty.txt
+project=$1
+echo "Evaluating $project.."
+java -cp target/pronto-evaluation-0.1-SNAPSHOT.jar de.peass.evaluation.infinitest.InfinitestEvaluator -folder ../../projekte/$project/ &> "$project"_infinitest.txt
+java -cp target/pronto-evaluation-0.1-SNAPSHOT.jar de.peass.evaluation.ekstazi.EkstaziEvaluator -folder ../../projekte/$project/ &> "$project"_ekstazi.txt
+java -cp target/pronto-evaluation-0.1-SNAPSHOT.jar de.peass.evaluation.empty.EmptyEvaluator -folder ../../projekte/$project/ &> "$project"_empty.txt
+
