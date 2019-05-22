251b849df5bb7a9477097082c32c4c94afbf6f53
David Georg Reichelt <davidgeorg_reichelt@dagere.de>
2019-05-22 11:23:06 +0200

    Ausgaben aufgeräumt

--- evaluateAll.sh
+++ evaluateAll.sh
@@ -1,8 +1,8 @@
 project=$1
 echo "Evaluating $project.."
-git -C ../../projekte/$project/ reset --hard
-git -C ../../projekte/$project/ clean -df
-git -C ../../projekte/$project/ pull origin master
+git -C ../../projekte/$project/ reset --hard &> "$project"_reset.txt
+git -C ../../projekte/$project/ clean -df &> "$project"_clean.txt
+git -C ../../projekte/$project/ pull origin master &> "$project"_pull.txt
 java -cp target/pronto-evaluation-0.1-SNAPSHOT.jar de.peass.evaluation.infinitest.InfinitestEvaluator -folder ../../projekte/$project/ &> "$project"_infinitest.txt
 java -cp target/pronto-evaluation-0.1-SNAPSHOT.jar de.peass.evaluation.ekstazi.EkstaziEvaluator -folder ../../projekte/$project/ &> "$project"_ekstazi.txt
 java -cp target/pronto-evaluation-0.1-SNAPSHOT.jar de.peass.evaluation.empty.EmptyEvaluator -folder ../../projekte/$project/ &> "$project"_empty.txt
