93f0de641fba3ee25ba3e5bfccff3071624c9b03
David Georg Reichelt <davidgeorg_reichelt@dagere.de>
2019-05-22 11:09:32 +0200

    Pull vor analyse

--- evaluateAll.sh
+++ evaluateAll.sh
@@ -1,5 +1,6 @@
 project=$1
 echo "Evaluating $project.."
+git --git-dir ../../projekte/$project/ pull
 java -cp target/pronto-evaluation-0.1-SNAPSHOT.jar de.peass.evaluation.infinitest.InfinitestEvaluator -folder ../../projekte/$project/ &> "$project"_infinitest.txt
 java -cp target/pronto-evaluation-0.1-SNAPSHOT.jar de.peass.evaluation.ekstazi.EkstaziEvaluator -folder ../../projekte/$project/ &> "$project"_ekstazi.txt
 java -cp target/pronto-evaluation-0.1-SNAPSHOT.jar de.peass.evaluation.empty.EmptyEvaluator -folder ../../projekte/$project/ &> "$project"_empty.txt
